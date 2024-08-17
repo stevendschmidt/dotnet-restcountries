9be3feaf74a132780449765333ed36faa7de0323
Steven Schmidt <stevendschmidt@gmail.com>
2024-08-17 15:11:20 -0400

    Removing route prefix

--- DotNetRestCountries/Program.cs
+++ DotNetRestCountries/Program.cs
@@ -31,7 +31,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
